8d1f5848228dc96937a5326dea04a6e325b61263
Thomas Zachariah <tzachari@umich.edu>
2014-09-22 10:46:06 -0400

    Outputs for LAMP pass (definitely incorrect)

--- HW1/sample/lib/hw1pass2/Release+Asserts/hw1pass2.d
+++ HW1/sample/lib/hw1pass2/Release+Asserts/hw1pass2.d
@@ -1,37 +1,32 @@
 /home/tzachari/HW1/sample/lib/hw1pass2/Release+Asserts/hw1pass2.o \
   /home/tzachari/HW1/sample/lib/hw1pass2/Release+Asserts/hw1pass2.d: \
-  hw1pass2.cpp /opt/llvm-source/include/llvm/Pass.h \
+  hw1pass2.cpp /opt/llvm-source/include/llvm/IR/Module.h \
+  /opt/llvm-source/include/llvm/ADT/OwningPtr.h \
   /opt/llvm-source/include/llvm/Support/Compiler.h \
   /opt/llvm/include/llvm/Config/llvm-config.h \
-  /opt/llvm-source/include/llvm/PassSupport.h \
-  /opt/llvm-source/include/llvm/InitializePasses.h \
-  /opt/llvm-source/include/llvm/PassRegistry.h \
-  /opt/llvm-source/include/llvm/ADT/StringRef.h \
-  /opt/llvm-source/include/llvm/Support/type_traits.h \
-  /opt/llvm/include/llvm/Support/DataTypes.h \
-  /opt/llvm-source/include/llvm/Support/CBindingWrapping.h \
-  /opt/llvm-source/include/llvm/Support/Casting.h \
-  /opt/llvm-source/include/llvm-c/Core.h \
-  /opt/llvm-source/include/llvm/Support/Atomic.h \
-  /opt/llvm-source/include/llvm/Support/Valgrind.h \
-  /opt/llvm-source/include/llvm/PassAnalysisSupport.h \
-  /opt/llvm-source/include/llvm/ADT/SmallVector.h \
-  /opt/llvm-source/include/llvm/Support/AlignOf.h \
-  /opt/llvm-source/include/llvm/Support/MathExtras.h \
-  /opt/llvm-source/include/llvm/Support/SwapByteOrder.h \
   /opt/llvm-source/include/llvm/IR/Function.h \
   /opt/llvm-source/include/llvm/IR/Argument.h \
   /opt/llvm-source/include/llvm/ADT/Twine.h \
+  /opt/llvm-source/include/llvm/ADT/StringRef.h \
+  /opt/llvm-source/include/llvm/Support/type_traits.h \
+  /opt/llvm/include/llvm/Support/DataTypes.h \
   /opt/llvm-source/include/llvm/Support/ErrorHandling.h \
   /opt/llvm-source/include/llvm/ADT/ilist_node.h \
   /opt/llvm-source/include/llvm/IR/Attributes.h \
   /opt/llvm-source/include/llvm/ADT/ArrayRef.h \
   /opt/llvm-source/include/llvm/ADT/None.h \
+  /opt/llvm-source/include/llvm/ADT/SmallVector.h \
+  /opt/llvm-source/include/llvm/Support/AlignOf.h \
+  /opt/llvm-source/include/llvm/Support/MathExtras.h \
+  /opt/llvm-source/include/llvm/Support/SwapByteOrder.h \
   /opt/llvm-source/include/llvm/ADT/FoldingSet.h \
   /opt/llvm-source/include/llvm/Support/PointerLikeTypeTraits.h \
   /opt/llvm-source/include/llvm/IR/Value.h \
   /opt/llvm-source/include/llvm/IR/Use.h \
   /opt/llvm-source/include/llvm/ADT/PointerIntPair.h \
+  /opt/llvm-source/include/llvm/Support/CBindingWrapping.h \
+  /opt/llvm-source/include/llvm/Support/Casting.h \
+  /opt/llvm-source/include/llvm-c/Core.h \
   /opt/llvm-source/include/llvm/IR/BasicBlock.h \
   /opt/llvm-source/include/llvm/ADT/ilist.h \
   /opt/llvm-source/include/llvm/IR/Instruction.h \
@@ -46,54 +41,65 @@
   /opt/llvm-source/include/llvm/IR/Type.h \
   /opt/llvm-source/include/llvm/ADT/APFloat.h \
   /opt/llvm-source/include/llvm/ADT/APInt.h \
+  /opt/llvm-source/include/llvm/IR/GlobalAlias.h \
+  /opt/llvm-source/include/llvm/IR/OperandTraits.h \
+  /opt/llvm-source/include/llvm/IR/GlobalVariable.h \
+  /opt/llvm-source/include/llvm/IR/Metadata.h \
+  /opt/llvm-source/include/llvm/Pass.h \
+  /opt/llvm-source/include/llvm/PassSupport.h \
+  /opt/llvm-source/include/llvm/InitializePasses.h \
+  /opt/llvm-source/include/llvm/PassRegistry.h \
+  /opt/llvm-source/include/llvm/Support/Atomic.h \
+  /opt/llvm-source/include/llvm/Support/Valgrind.h \
+  /opt/llvm-source/include/llvm/PassAnalysisSupport.h \
+  /opt/llvm-source/include/llvm/Analysis/Passes.h \
+  /opt/llvm-source/include/llvm/Analysis/LoopPass.h \
+  /opt/llvm-source/include/llvm/Analysis/LoopInfo.h \
+  /opt/llvm-source/include/llvm/ADT/DenseMap.h \
+  /opt/llvm-source/include/llvm/ADT/DenseMapInfo.h \
+  /opt/llvm-source/include/llvm/ADT/DenseSet.h \
+  /opt/llvm-source/include/llvm/ADT/GraphTraits.h \
+  /opt/llvm-source/include/llvm/Analysis/Dominators.h \
+  /opt/llvm-source/include/llvm/ADT/DepthFirstIterator.h \
+  /opt/llvm-source/include/llvm/ADT/SmallPtrSet.h \
+  /opt/llvm-source/include/llvm/Support/CFG.h \
+  /opt/llvm-source/include/llvm/IR/InstrTypes.h \
   /opt/llvm-source/include/llvm/Support/raw_ostream.h \
-  /opt/llvm-source/include/llvm/Analysis/ProfileInfo.h \
+  /opt/llvm-source/include/llvm/PassManagers.h \
+  /opt/llvm-source/include/llvm/Support/PrettyStackTrace.h \
+  /opt/llvm-source/include/llvm/IR/Constants.h \
+  /opt/llvm-source/include/llvm/IR/Instructions.h \
+  /opt/llvm-source/include/llvm/Support/IntegersSubset.h \
+  /opt/llvm-source/include/llvm/IR/LLVMContext.h \
+  /opt/llvm-source/include/llvm/Support/IntegersSubsetMapping.h \
   /opt/llvm-source/include/llvm/Support/Debug.h \
-  /opt/llvm-source/include/llvm/Support/Format.h
+  /opt/llvm-source/include/llvm/IR/DataLayout.h \
+  /opt/llvm-source/include/llvm/ADT/IndexedMap.h \
+  /opt/llvm-source/include/llvm/ADT/STLExtras.h \
+  /opt/llvm-source/include/llvm/Analysis/ProfileInfo.h \
+  /opt/llvm-source/include/llvm/Support/Format.h \
+  /home/tzachari/HW1/sample/lib/hw1pass2/LAMP/LAMPLoadProfile.h \
+  /home/tzachari/HW1/sample/lib/hw1pass2/LAMP/LAMPProfiling.h
 
-/opt/llvm-source/include/llvm/Pass.h:
+/opt/llvm-source/include/llvm/IR/Module.h:
+
+/opt/llvm-source/include/llvm/ADT/OwningPtr.h:
 
 /opt/llvm-source/include/llvm/Support/Compiler.h:
 
 /opt/llvm/include/llvm/Config/llvm-config.h:
-
-/opt/llvm-source/include/llvm/PassSupport.h:
-
-/opt/llvm-source/include/llvm/InitializePasses.h:
-
-/opt/llvm-source/include/llvm/PassRegistry.h:
-
-/opt/llvm-source/include/llvm/ADT/StringRef.h:
-
-/opt/llvm-source/include/llvm/Support/type_traits.h:
-
-/opt/llvm/include/llvm/Support/DataTypes.h:
-
-/opt/llvm-source/include/llvm/Support/CBindingWrapping.h:
-
-/opt/llvm-source/include/llvm/Support/Casting.h:
-
-/opt/llvm-source/include/llvm-c/Core.h:
-
-/opt/llvm-source/include/llvm/Support/Atomic.h:
-
-/opt/llvm-source/include/llvm/Support/Valgrind.h:
-
-/opt/llvm-source/include/llvm/PassAnalysisSupport.h:
-
-/opt/llvm-source/include/llvm/ADT/SmallVector.h:
-
-/opt/llvm-source/include/llvm/Support/AlignOf.h:
-
-/opt/llvm-source/include/llvm/Support/MathExtras.h:
-
-/opt/llvm-source/include/llvm/Support/SwapByteOrder.h:
 
 /opt/llvm-source/include/llvm/IR/Function.h:
 
 /opt/llvm-source/include/llvm/IR/Argument.h:
 
 /opt/llvm-source/include/llvm/ADT/Twine.h:
+
+/opt/llvm-source/include/llvm/ADT/StringRef.h:
+
+/opt/llvm-source/include/llvm/Support/type_traits.h:
+
+/opt/llvm/include/llvm/Support/DataTypes.h:
 
 /opt/llvm-source/include/llvm/Support/ErrorHandling.h:
 
@@ -105,6 +111,14 @@
 
 /opt/llvm-source/include/llvm/ADT/None.h:
 
+/opt/llvm-source/include/llvm/ADT/SmallVector.h:
+
+/opt/llvm-source/include/llvm/Support/AlignOf.h:
+
+/opt/llvm-source/include/llvm/Support/MathExtras.h:
+
+/opt/llvm-source/include/llvm/Support/SwapByteOrder.h:
+
 /opt/llvm-source/include/llvm/ADT/FoldingSet.h:
 
 /opt/llvm-source/include/llvm/Support/PointerLikeTypeTraits.h:
@@ -114,6 +128,12 @@
 /opt/llvm-source/include/llvm/IR/Use.h:
 
 /opt/llvm-source/include/llvm/ADT/PointerIntPair.h:
+
+/opt/llvm-source/include/llvm/Support/CBindingWrapping.h:
+
+/opt/llvm-source/include/llvm/Support/Casting.h:
+
+/opt/llvm-source/include/llvm-c/Core.h:
 
 /opt/llvm-source/include/llvm/IR/BasicBlock.h:
 
@@ -143,10 +163,80 @@
 
 /opt/llvm-source/include/llvm/ADT/APInt.h:
 
+/opt/llvm-source/include/llvm/IR/GlobalAlias.h:
+
+/opt/llvm-source/include/llvm/IR/OperandTraits.h:
+
+/opt/llvm-source/include/llvm/IR/GlobalVariable.h:
+
+/opt/llvm-source/include/llvm/IR/Metadata.h:
+
+/opt/llvm-source/include/llvm/Pass.h:
+
+/opt/llvm-source/include/llvm/PassSupport.h:
+
+/opt/llvm-source/include/llvm/InitializePasses.h:
+
+/opt/llvm-source/include/llvm/PassRegistry.h:
+
+/opt/llvm-source/include/llvm/Support/Atomic.h:
+
+/opt/llvm-source/include/llvm/Support/Valgrind.h:
+
+/opt/llvm-source/include/llvm/PassAnalysisSupport.h:
+
+/opt/llvm-source/include/llvm/Analysis/Passes.h:
+
+/opt/llvm-source/include/llvm/Analysis/LoopPass.h:
+
+/opt/llvm-source/include/llvm/Analysis/LoopInfo.h:
+
+/opt/llvm-source/include/llvm/ADT/DenseMap.h:
+
+/opt/llvm-source/include/llvm/ADT/DenseMapInfo.h:
+
+/opt/llvm-source/include/llvm/ADT/DenseSet.h:
+
+/opt/llvm-source/include/llvm/ADT/GraphTraits.h:
+
+/opt/llvm-source/include/llvm/Analysis/Dominators.h:
+
+/opt/llvm-source/include/llvm/ADT/DepthFirstIterator.h:
+
+/opt/llvm-source/include/llvm/ADT/SmallPtrSet.h:
+
+/opt/llvm-source/include/llvm/Support/CFG.h:
+
+/opt/llvm-source/include/llvm/IR/InstrTypes.h:
+
 /opt/llvm-source/include/llvm/Support/raw_ostream.h:
 
-/opt/llvm-source/include/llvm/Analysis/ProfileInfo.h:
+/opt/llvm-source/include/llvm/PassManagers.h:
+
+/opt/llvm-source/include/llvm/Support/PrettyStackTrace.h:
+
+/opt/llvm-source/include/llvm/IR/Constants.h:
+
+/opt/llvm-source/include/llvm/IR/Instructions.h:
+
+/opt/llvm-source/include/llvm/Support/IntegersSubset.h:
+
+/opt/llvm-source/include/llvm/IR/LLVMContext.h:
+
+/opt/llvm-source/include/llvm/Support/IntegersSubsetMapping.h:
 
 /opt/llvm-source/include/llvm/Support/Debug.h:
 
+/opt/llvm-source/include/llvm/IR/DataLayout.h:
+
+/opt/llvm-source/include/llvm/ADT/IndexedMap.h:
+
+/opt/llvm-source/include/llvm/ADT/STLExtras.h:
+
+/opt/llvm-source/include/llvm/Analysis/ProfileInfo.h:
+
 /opt/llvm-source/include/llvm/Support/Format.h:
+
+/home/tzachari/HW1/sample/lib/hw1pass2/LAMP/LAMPLoadProfile.h:
+
+/home/tzachari/HW1/sample/lib/hw1pass2/LAMP/LAMPProfiling.h:
